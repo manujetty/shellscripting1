f8103850b4761c58b7c69f38279f2e01b7c472b8
manujetty <manupraneeth2@gmail.com>
2020-07-01 12:03:25 +0530

    new-shell script-project

---
+++ project/roboshop.sh
@@ -0,0 +1,38 @@
+pwd
+
+
+exit
+
+B="\e[1;34m"
+Y="\e[1;33m"
+R="\e[1;31m"
+N="\e[0m"
+
+PRINT() {
+  echo -e "[${B}INF${N}] $1"
+}
+
+ERROR() {
+  echo -e "[${R}ERROR${N}] $1"
+}
+
+COMPONENT=$1
+
+if [[ -z "${COMPONENT}" ]];then
+  INSTALL_COMPONENT="MONGODB MYSQL REDIS  RABBITMQ FRONTEND CART CATALOGUE USER PAYMENT SHIPPING RATING DISPATCH"
+else
+  case ${COMPONENT} in
+    MONGODB|MYSQL|REDIS|RABBITMQ|FRONTEND|CART|CATALOGUE|USER|PAYMENT|SHIPPING|RATING|DISPATCH)
+      INSTALL_COMPONENT=${COMPONENT}
+      ;;
+     *)
+      ERROR "INPUT NOT SUPPORTED"
+      exit 1
+      ;;
+  esac
+fi
+
+for component in ${INSTALL_COMPONENT};do
+  PRINT "SETUP ${component}"
+  sh $component.sh
+done
