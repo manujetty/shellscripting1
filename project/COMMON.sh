a0be642831938c35f6c8c159f0233be5e46bc046
manujetty <manupraneeth2@gmail.com>
2020-07-18 19:33:23 +0530

    new-shell script-project

--- project/COMMON.sh
+++ project/COMMON.sh
@@ -2,12 +2,12 @@ B="\e[1;34m"
 Y="\e[1;33m"
 R="\e[1;31m"
 N="\e[0m"
-
+C="\e[1,36m"
 
 PRINT() {
   echo -e "[${B}INF${N}] $1"
 }
 
-ERROR() {
-  echo -e "[${R}ERROR${N}] $1"
+HEAD() {
+  echo -e "[${R}INF${N}] ${C}֍֍ $1 ֍֍ ${N}"
 }
--- project/roboshop.sh
+++ project/roboshop.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
-B="\e[1;34m"
-Y="\e[1;33m"
-R="\e[1;31m"
-N="\e[0m"
 
-
-PRINT() {
-  echo -e "[${B}INF${N}] $1"
-}
-
-ERROR() {
-  echo -e "[${R}ERROR${N}] $1"
-}
+source $(dirname $0)/COMMON.sh
 
 COMPONENT=$1
 
